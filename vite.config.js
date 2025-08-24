8c7010a955c1b970072db56ab6d1c9dc82099a8b
Irfan-riyaz <133787187+Irfan-riyaz@users.noreply.github.com>
2025-08-24 13:18:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/furniture-placement/', // ⬅️ EXACTLY your GitHub repo name, with slashes
   plugins: [react()],
-  server:{
-    host:true,
-    port:3000,
-    watch:{
-      usePolling:true
-    }
-  }
 })
